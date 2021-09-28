80f706552fdc37f40a6dfc8f7834e75d326b4c30
Yana Nechveeva <79226190+Yana1112pstr@users.noreply.github.com>
2021-09-28 09:23:24 +0300

    готовая работа

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import './sass/main.scss';
-//import debounce from 'lodash/debounce ';
 import fetchCountries from './js/fetchCountries';
-
+import countryList from './templates/country-list.hbs'
 import countryCardTpl from './templates/country-card.hbs';
+import debounce from 'lodash.debounce';
 
 const refs = {
-    searchQuery : document.querySelector('#searchQuery')
+    searchQuery : document.querySelector('#input'),
+    countryList : document.querySelector('.js-articles'),
+    wrapper : document.querySelector('.wrapper')
 }
 
-    refs.searchQuery.addEventListener('change',(e) => {
-        e.preventDefault()
-        console.log(e.target.value)
-        const {value} = e.target.value
-        fetchCountries(value) ;
-        e.target.value = '';
-    } ) ;
+    refs.searchQuery.addEventListener('input',debounce(onSearch, 500)) ;
 
-    
-//     function onSearch(e) {
-//         e.preventDefault()
-//         newFetchCountry.query = e.currentTarget.elements.query.value;
-//         newFetchCountry.fetchCountries(searchQuery);
-//     }
+    function onSearch(e) {
+        refs.countryList.innerHTML = '';
+        const userRequest = e.target.value.trim()
+        if (userRequest.length < 1) return;
+        console.log(userRequest)
+        fetchCountries(userRequest).then(country => {
+            if (!country) return;
+            if (country.length > 1 && country.length <=10 ) {
+                return (refs.countryList.innerHTML = countryList(country))
+            }
+            if (country.length === 1) {
+                return (refs.wrapper.innerHTML = countryCardTpl(...country))
+            }
+            alert('Too many matches found. Please enter a more specific query!')
+        })
+    }
+
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,32 +1,14 @@
-export default  function fetchCountries(searchQuery) { 
-        return fetch(`https://restcountries.com/v3/name/${searchQuery}`)
-        .then (response => {
-            if (response.status === 200) {
-                return response.json()
-            }
-            else {
-                throw new Error('Error')
-            }
-        } )
-        .then ((data) => {
-            return data;
-        })
-        .catch (error => {console.log(error);})
-    }
-
-
-        // .then ((array) => {
-        //     let result = array.map((item) => {
-        //         console.log(item.name);
-        //         return `<li>${item.name}</li>`;
-        //     })
-        // console.log(result.join(''));
-        // return result.join('');
-        // })
-
-    // get query() {
-    //     return searchQuery;
-    // }
-    // set query(newquery) {
-    //     searchQuery = newquery;
-    // }
+export default function fetchCountries(searchQuery) {
+  if (searchQuery) {
+    return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Error');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
